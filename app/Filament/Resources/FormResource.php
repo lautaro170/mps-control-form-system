e54ac0a17ccfb3c487cb4d39fe47aa94e2bda58a
Lautaro Ybarra <lautaroybarra1@gmail.com>
2025-08-06 15:23:36 -0300

    feature/ Changed default order of FormResource to be date desc

--- app/Filament/Resources/FormResource.php
+++ app/Filament/Resources/FormResource.php
@@ -166,7 +166,8 @@ class FormResource extends Resource
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
-            ]);
+            ])
+            ->defaultSort('created_at', 'desc');
     }
 
     public static function getRelations(): array
